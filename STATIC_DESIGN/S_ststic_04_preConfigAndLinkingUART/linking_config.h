6569612308cbc9e6d0cc2e6bdcd5f9b02ca869fb
Sharpel Malak <sharbelmalak@gmail.com>
2023-05-11 17:51:18 +0200

    adding linking file.h

---
+++ STATIC_DESIGN/S_ststic_04_preConfigAndLinkingUART/linking_config.h
@@ -0,0 +1,48 @@
+/*
+ * linking_config.h
+ *
+ * Created: 5/11/2023 4:50:10 PM
+ *  Author: sharpel
+ */ 
+
+
+#ifndef USART_LINKING_H_
+#define USART_LINKING_H_
+
+
+typedef enum EN_FCPU_SELECTION{
+    FCPU1=0,
+    FCPU2,
+    FCPU4,
+    FCPU8,
+
+}EN_FCPU_SELECTION;
+
+typedef enum EN_BAUDRATE_SELECTION{
+    BAUDRATE_2400=0,
+    BAUDRATE_4600,
+    BAUDRATE_9600,
+    
+
+}EN_BAUDRATE_SELECTION;
+
+
+typedef enum EN_DATASIZE_SELECTION{
+    USART_DATA_SIZE_5=0,
+    USART_DATA_SIZE_6,
+    USART_DATA_SIZE_7,
+    USART_DATA_SIZE_8,
+    USART_DATA_SIZE_9,
+
+}EN_DATASIZE_SELECTION;
+
+
+typedef struct ST_USART_CONF{
+    EN_FCPU_SELECTION FCPU;
+    EN_BAUDRATE_SELECTION BAUDRATE;
+    EN_DATASIZE_SELECTION DATASIZE;
+}ST_USART_CONF;
+
+
+
+#endif 
